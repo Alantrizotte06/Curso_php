1887af9f61f8c92a0588541edd137562d4a2ee02
Alantrizotte06 <trizotte.alan@gmail.com>
2024-06-04 20:39:38 -0300

    Testando ambiente em PHP

---
+++ Ex00/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programa 1</title>
+</head>
+
+<body>
+  <h1>
+    <?php 
+      //para exibir algo em tela pode ser utilizado print também.
+      echo "Olá mundo! \u{1F30E}"; // \u é utilizado para representar um caracter em unicode.
+    ?>
+  </h1>
+  <p>Testando php</p>
+</body>
+
+</html>
