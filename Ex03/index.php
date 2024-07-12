eef4a1db408ef6619d4a426737191387211cc12f
Alantrizotte06 <trizotte.alan@gmail.com>
2024-07-12 13:49:16 -0300

    Exercicio 3 - Exemplo de vetor e função var_dump

---
+++ Ex03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php 
+    
+    $vet = [0,6,9.3,8];
+
+    var_dump($vet[2]);
+  ?>
+</body>
+
+</html>
